9ac7a743e17497b4e97dedac723c6f6733ea2164
Anshul Prasad <anshulprasad5440.com>
2026-03-16 17:17:29 +0530

    variable name change. encoder is added. structural changes.

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
 from pathlib import Path
+import tiktoken
 
-CHANNEL_URLS = [
-    "https://www.youtube.com/@CHANNEL_ID_1",
-    "https://www.youtube.com/@CHANNEL_ID_2",
+CHANNEL_IDS = [
+    "CHANNEL_ID_1",
+    "CHANNEL_ID_2",
 ]
 
 VTT_DIR    = Path("data/subtitles_vtt")
 TXT_DIR    = Path("data/transcripts_txt")
-FILE_PATHS = Path("data/file_paths.pkl")
-TRANSCRIPTS = Path("data/transcripts.pkl")
-TRANSCRIPT_INDEX = "data/transcript_index.faiss"   # FILE not dir
-CHUNKS_PKL = "data/chunks.pkl"                     # ← new: chunked texts
+FILE_PKL = Path("data/files_pkl.pkl")
+TRANSCRIPTS_PKL = Path("data/transcripts_pkl.pkl")
+CHUNK_FAISS = Path("data/chunks_faiss.faiss")
+CHUNK_PKL = Path("data/chunks_pkl.pkl")
 
 RETRIEVED_TRANSCRIPTS_FILE = Path("outputs/retrieved_transcripts.txt")
 RESPONSE_FILE              = Path("outputs/generated_response.txt")
@@ -20,6 +21,13 @@ COOKIES_FILE               = Path("src/youtube_cookies.txt")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 MODEL        = "llama-3.1-8b-instant"
 MAX_CONTEXT_TOKENS = 4500
+
+# Token
+try:
+    ENCODER = tiktoken.encoding_for_model(MODEL)
+except KeyError:
+    # fallback for custom or unrecognized model names
+    ENCODER = tiktoken.get_encoding("cl100k_base")
 
 SYSTEM_PROMPT = """
 You are speaking as Spiritual Guru.
@@ -36,7 +44,7 @@ Guidelines:
 - Challenge false assumptions in the question when necessary.
 - Do not claim personal authority; emphasize inquiry and self-observation.
 - Avoid religious ritualism; focus on inner understanding.
-- Do not reference yourself as an AI or model.
+- Do not reference yourself as an AI or model and don't reveal the guru's name if you by chance know from the given prompt.
 - Do not mention that you are imitating someone.
 - If the context is insufficient, say so plainly instead of guessing.
 - Answer questions strictly using the provided context.
